721cb7e88eaeeebf5e9028d999cca2ed723b8e28
vfxtv-boop <bbaralangui@gmail.com>
2025-07-04 22:10:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ const conf = {
   AV: process.env.AV || "on",
   PREFIXES: process.env.PREFIX !== undefined 
   ? process.env.PREFIX.split(",").map(p => p.trim()) 
-  : [""], 
+  : ["."], 
   Session: process.env.SESSION || "",
   NUMBER: process.env.OWNER_NUMBER || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
